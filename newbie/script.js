c092c34fc0299cc1ba4acd21a6016884d260143d
Melvin <melsava1207@gmail.com>
2025-04-09 18:49:47 -0600

    Pagina creada solo falta agregaar boton de retorno en newbie

---
+++ newbie/script.js
@@ -0,0 +1,56 @@
+const section = document.querySelector('.projects')
+
+fetch('./projects.json')
+.then(res => res.json())
+    .then(data =>{
+        createCards(data)
+    })
+    .catch(err => console.error(err))
+
+
+function createCards(datos){
+    section.innerHTML = ""
+
+    datos.forEach(({ title, image, url, technologies }) => {
+        let images = ''
+
+        technologies.forEach(tech => {
+            if(tech == "html"){
+                images += `
+                    <img class="tech__logo" src=${'./assets/html.svg'} alt='${tech}'/>
+                `
+            } else if(tech == "css"){
+                images += `
+                    <img class="tech__logo" src=${'./assets/css.svg'} alt='${tech}' />
+                `
+            } else if(tech == "js"){
+                images += `
+                    <img class="tech__logo" src=${'./assets/js.svg'} alt='${tech}' />
+                `
+            } else if(tech == "sass"){
+                images += `
+                    <img class="tech__logo" src=${'./assets/sass.svg'} alt='${tech}' />
+                `
+            } else if(tech == "tailwind"){
+                images += `
+                    <img class="tech__logo" src=${'./assets/tailwind.svg'} alt='${tech}' />
+                `
+            }
+        })
+
+
+        const articulo = document.createElement('a')
+        articulo.setAttribute('href', url)
+        articulo.innerHTML = `
+            <article href=${url} class="card">
+                <img class="card__img" src=${image} alt=${title}/>
+                <h2 class="card__title">${title}</h2>
+                <div class="tech__logos">
+                    ${images}
+                </div>
+            </article>
+        `
+
+        section.appendChild(articulo)
+    })
+}
